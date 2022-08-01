a1009e65e82b8c933a4bbff097ae6c6c375346c5
jiangtong <jiangtong@xiaomi.com>
2022-08-01 23:21:19 +0800

    fix bug

--- packages/components/rollup.config.js
+++ packages/components/rollup.config.js
@@ -1,11 +1,11 @@
 import path from 'path';
 import packageJson from './package.json';
-// import esbuild from 'rollup-plugin-esbuild';
+import esbuild from 'rollup-plugin-esbuild';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import json from '@rollup/plugin-json';
 import commonjs from '@rollup/plugin-commonjs';
-// import replace from '@rollup/plugin-replace';
-import babel from '@rollup/plugin-babel';
+import replace from '@rollup/plugin-replace';
+import babel, { getBabelOutputPlugin } from '@rollup/plugin-babel';
 // import eslint from '@rollup/plugin-eslint';
 // import { getBabelOutputPlugin } from '@rollup/plugin-babel';
 // import typescript from '@rollup/plugin-typescript';
@@ -24,30 +24,30 @@ export default {
     plugins: [
         // getBabelOutputPlugin({
         //     exclude: ['node_modules/**', 'dist/**/*'],
+        //     extensions: ['.ts', '.tsx', '.js', '.jsx'],
         //     presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
         // }),
 
-        babel({
-            exclude: ['node_modules/**']
-        }),
+        // babel({
+        //     extensions: ['.ts', '.tsx', '.js', '.jsx'],
+        //     exclude: ['node_modules/**']
+        // }),
 
-        // typescript({ exclude: ['node_modules/**'] }),
+        // typescript({ tsconfig: './tsconfig.json', exclude: ['node_modules/**'] }),
 
         // eslint({
         //     exclude: ['node_modules/**']
         // }),
 
-        typescript(),
-
-        // esbuild({
-        //     include: /\.[jt]sx?$/,
-        //     exclude: /node_modules/,
-        //     sourceMap: false,
-        //     jsxFactory: 'React.createElement',
-        //     jsxFragment: 'React.Fragment',
-        //     target: 'esnext',
-        //     tsconfig: './tsconfig.json' // default
-        // }),
+        esbuild({
+            include: /\.[jt]sx?$/,
+            exclude: /node_modules/,
+            sourceMap: false,
+            jsxFactory: 'React.createElement',
+            jsxFragment: 'React.Fragment',
+            target: 'esnext',
+            tsconfig: './tsconfig.json' // default
+        }),
 
         nodeResolve({
             extensions: ['.tsx', '.ts', '.jsx', '.js']
@@ -57,11 +57,11 @@ export default {
 
         commonjs({
             include: ['node_modules/**']
-        })
+        }),
 
-        // replace({
-        //     'process.env.NODE_ENV': JSON.stringify('development')
-        // })
+        replace({
+            'process.env.NODE_ENV': JSON.stringify('development')
+        })
     ],
-    external: packageJson.peerDependencies
+    external: Object.keys(packageJson.peerDependencies)
 };
